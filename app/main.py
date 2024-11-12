a77ca0228ce655c589cb35fc5ee5d063d39f91c1
r3t4rd <153238796+r3t4rd2227@users.noreply.github.com>
2024-11-12 16:48:02 +0000

    main.py file updated, docker image for fastapi app updated

--- app/main.py
+++ app/main.py
@@ -11,7 +11,5 @@ def get_time():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000,
-#    ssl_certfile="/etc/letsencrypt/live/jbadeyi.xyz/fullchain.pem",
-#    ssl_keyfile="/etc/letsencrypt/live/jbadeyi.xyz/privkey.pem")
+    uvicorn.run(app, host="0.0.0.0", port=5000
 )
